14b26c3c3994dcabc70e334aaae0030eff16214e
cca-dev <41578202+cca-dev@users.noreply.github.com>
2018-08-08 13:15:55 +0100

    Update ai-external.js

--- ai-external.js
+++ ai-external.js
@@ -1,6 +1,6 @@
 /**
  *  Advanced iframe pro external workaround file v7.5.x
- *  Created: 2018-08-08 12:04:38
+ *  Created: 2018-08-08 12:14:47
 */
 /* jslint devel: true, evil: false */
 if (typeof domain_advanced_iframe === 'undefined') {
@@ -750,7 +750,7 @@ if (doIt) {
       var write_css_directly = 'false';
   }
   if (typeof resize_on_element_resize === 'undefined') {
-      var resize_on_element_resize = 'div#Wrapper';
+      var resize_on_element_resize = 'div#top_container';
   }
   if (typeof resize_on_element_resize_delay === 'undefined') {
       var resize_on_element_resize_delay = '100';
